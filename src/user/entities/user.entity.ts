c6066e2d07fe7364057c64924039a8d919bf4015
Daniel González <qbixmex@gmail.com>
2026-08-28 18:04:20 -0600

    TypeORM UserEntity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,14 +1,72 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
 export class User {
+  @PrimaryGeneratedColumn('uuid')
   id!: string;
+
+  @Column({
+    type: 'varchar',
+    name: 'name',
+    length: 200,
+  })
   name!: string;
+
+  @Column({
+    type: 'varchar',
+    name: 'username',
+    length: 200,
+  })
   username?: string;
+
+  @Column({
+    type: 'varchar',
+    name: 'email',
+  })
   email!: string;
+
+  @Column({
+    type: 'boolean',
+    default: false,
+    nullable: true,
+  })
   emailVerified?: boolean;
+
+  @Column({
+    type: 'varchar',
+    name: 'image_url',
+    nullable: true,
+  })
   imageUrl?: string;
+
+  @Column({
+    type: 'varchar',
+    name: 'image_public_id',
+    nullable: true,
+  })
   imagePublicId?: string;
+
+  @Column({
+    type: 'varchar',
+    name: 'password',
+  })
   password!: string;
+
+  @Column({
+    type: 'boolean',
+    name: 'is_active',
+    default: false,
+  })
   isActive?: boolean;
 
+  @CreateDateColumn({
+    name: 'created_at',
+  })
   createdAt!: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_date',
+    nullable: true,
+  })
   updatedAt?: Date;
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([User])
+  ],
 })
 export class UserModule {}
