4bc644b09a5c64929108732f4710608fd28201b7
Daniel González Briseño <qbixmex@gmail.com>
2026-09-06 01:09:57 -0600

    Fixed root route
    - Removed verce.json
    - Set Global prefix to exclude "/" and "health"
    - Added skip-transform decorator for skip transform response
    - Version dumped

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', { exclude: ['/', 'health'] });
   app.enableVersioning({ type: VersioningType.URI });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
